0c34385385f6b399cadfd1bd186982856eeed7c1
sungwoo <62142688+sw-woo@users.noreply.github.com>
2024-10-21 22:00:01 +0900

    Update rag_advanced_streamlit.py
    faiss로 vector DB 변경

--- rag_advanced_streamlit.py
+++ rag_advanced_streamlit.py
@@ -8,27 +8,28 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain_chroma import Chroma
-from langchain.vectorstores import Chroma
+from langchain_chroma import Chroma
+# 베포시 chromadb 문제로 아래 api 사용
+# from langchain.vectorstores import Chroma
 from langchain.retrievers import BM25Retriever, EnsembleRetriever
 from langchain_core.output_parsers import StrOutputParser
 from streamlit_extras.buy_me_a_coffee import button
 from langchain.load import dumps, loads
+from langchain_community.vectorstores import FAISS
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
 
-# 베포시 chroma db에서 sqlite3를 사용하는데 오류가 나서 추가하였다.
-import sys
-__import__('pysqlite3')
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-# Initialize buy me a coffee button
+
+# 'Buy me a coffee' 버튼 초기화 username에 각자 username을 입력해주세요!
 button(username="swpheus14", floating=True, width=221)
 
-# Set the title and instructions
+# 제목과 지침 설정
 st.title("ChatPDF with Multiquery+hybridSearch+RagFusion")
 st.write("---")
 st.write("PDF 파일을 업로드하고 내용을 기반으로 질문하세요.")
 
-# OpenAI API key input
+# OpenAI API 키 입력
 openai_key = st.text_input('OpenAI API 키를 입력해 주세요!', type="password")
 
 # GPT 모델 선택
@@ -37,13 +38,11 @@ model_choice = st.selectbox(
     ['gpt-3.5-turbo', 'gpt-4o-mini', 'gpt-4o']
 )
 
-# File upload
+# 파일 업로드
 uploaded_file = st.file_uploader("PDF 파일을 업로드해 주세요!", type=['pdf'])
 st.write("---")
 
-# Function to convert PDF to documents
-
-
+# PDF를 문서로 변환하는 함수
 def pdf_to_document(uploaded_file):
     temp_dir = tempfile.TemporaryDirectory()
     temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
@@ -53,41 +52,58 @@ def pdf_to_document(uploaded_file):
     pages = loader.load_and_split()
     return pages
 
-# Function to format documents
-
-
+# 문서를 포맷하는 함수
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 
-# Check if file is uploaded
+# 파일이 업로드되었는지 확인
 if uploaded_file is not None:
     pages = pdf_to_document(uploaded_file)
 
-    # Split
-    text_splitter = RecursiveCharacterTextSplitter(
+    # 문서를 청크로 분할
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=500,
-        chunk_overlap=20,
-        length_function=len,
-        is_separator_regex=False,
+        chunk_overlap=50
     )
+    # 베포시에는 chroma DB문제로 아래 split 사용
+    # Split
+    # text_splitter = RecursiveCharacterTextSplitter(
+    #     chunk_size=500,
+    #     chunk_overlap=20,
+    #     length_function=len,
+    #     is_separator_regex=False,
+    # )
     splits = text_splitter.split_documents(pages)
 
-    # Embeddings and Chroma setup
+    # 임베딩 및 Chroma 설정
     embeddings_model = OpenAIEmbeddings(openai_api_key=openai_key)
-    vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings_model)
-    chroma_retriever = vectorstore.as_retriever(search_kwargs={'k': 1})
+    index = faiss.IndexFlatL2(len(OpenAIEmbeddings().embed_query("hello world")))
 
-    # BM25 Retriever setup
+    vectorstore = FAISS(
+        embedding_function=embeddings_model,
+        index=index,
+        docstore=InMemoryDocstore(),
+        index_to_docstore_id={} 
+    )
+    vectorstore.add_documents(documents=splits, ids=range(len(splits)))
+    faiss_retriever = vectorstore.as_retriever(search_type="mmr",  # MMR 알고리즘을 사용하여 검색
+                                                # 상위 1개의 문서를 반환하지만, 고려할 문서는 4개로 설정
+                                                search_kwargs={'k': 1, 'fetch_k': 4})
+
+    # 베포시 chroma DB문제로 아래 선언 사용
+    # faiss_retriever = vectorstore.as_retriever(search_kwargs={'k': 1})
+
+    # BM25 리트리버 설정
     bm25_retriever = BM25Retriever.from_documents(splits)
     bm25_retriever.k = 2
 
-    # Ensemble Retriever setup
+    # 앙상블 리트리버 설정
     ensemble_retriever = EnsembleRetriever(
-        retrievers=[bm25_retriever, chroma_retriever], weights=[0.2, 0.8]
+        retrievers=[bm25_retriever, faiss_retriever], weights=[0.2, 0.8]
     )
 
-    # Generate queries for RAG-Fusion
+    # RAG-Fusion을 위한 쿼리 생성
     template = """
     당신은 AI 언어 모델 조수입니다. 주어진 사용자 질문에 대해 벡터 데이터베이스에서 관련 문서를 검색할 수 있도록 다섯 가지 다른 버전을 생성하는 것입니다. 
     사용자 질문에 대한 여러 관점을 생성함으로써, 거리 기반 유사성 검색의 한계를 극복하는 데 도움을 주는 것이 목표입니다. 
@@ -102,7 +118,7 @@ if uploaded_file is not None:
         | (lambda x: x.split("\n"))
     )
 
-    # Reciprocal Rank Fusion function
+    # Reciprocal Rank Fusion 함수
     def reciprocal_rank_fusion(results: list[list], k=60, top_n=2):
         fused_scores = {}
         for docs in results:
@@ -119,11 +135,10 @@ if uploaded_file is not None:
 
         return reranked_results[:top_n]
 
-    # RAG-Fusion Chain setup
-    retrieval_chain_rag_fusion = generate_queries | ensemble_retriever.map(
-    ) | reciprocal_rank_fusion
+    # RAG-Fusion 체인 설정
+    retrieval_chain_rag_fusion = generate_queries | ensemble_retriever.map() | reciprocal_rank_fusion
 
-    # Final RAG Chain setup
+    # 최종 RAG 체인 설정
     template = """다음 맥락을 바탕으로 질문에 답변하세요:
 
     {context}
@@ -141,7 +156,7 @@ if uploaded_file is not None:
         | StrOutputParser()
     )
 
-    # User question input
+    # 사용자 질문 입력
     st.header("PDF에 질문하세요!")
     question = st.text_input('질문을 입력하세요')
 
